39733f1ad6c8bac5648bfb5fe16a441f86c79285
Ricardo <ricindigus@gmail.com>
2017-01-19 10:29:12 -0500

    finalizado youtube y actionbar

--- app/src/main/java/com/example/apoyo03_ui/antidoppingapp/VideoActivity.java
+++ app/src/main/java/com/example/apoyo03_ui/antidoppingapp/VideoActivity.java
@@ -1,19 +1,38 @@
 package com.example.apoyo03_ui.antidoppingapp;
 
+import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
-public class VideoActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+public class VideoActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener,YouTubePlayer.PlaybackEventListener {
+    Toolbar toolbar;
+    YouTubePlayerView youTubePlayerView;
+    String claveyouTube = "AIzaSyCqrythE898NidQR9JfqQwZVBoMci0HKWk";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
-        showToolbar("¿Cómo es un doppaje?",true);
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubePlayerView.initialize(claveyouTube,this);
+        toolbar = (Toolbar) findViewById(R.id.toolbarApp);
+        toolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.video_primary));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
         if (android.os.Build.VERSION.SDK_INT >= 21) {
             Window window = this.getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
@@ -22,11 +41,63 @@ public class VideoActivity extends AppCompatActivity {
         }
     }
 
-    public void showToolbar(String title, boolean upButton){
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarApp);
-        toolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.video_primary));
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(title);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean fueRestaurado) {
+        if(!fueRestaurado){
+            youTubePlayer.cueVideo("BiMlEN7610o");
+        }
     }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if(youTubeInitializationResult.isUserRecoverableError()){
+            youTubeInitializationResult.getErrorDialog(this,1).show();
+        }else{
+            String error = "Error al iniciar Youtube" + youTubeInitializationResult.toString();
+            Toast.makeText(getApplication(), error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode == 1){
+            getYoutubePlayerProvider().initialize(claveyouTube,this);
+        }
+    }
+
+    protected YouTubePlayer.Provider getYoutubePlayerProvider(){
+        return youTubePlayerView;
+    }
+
+    @Override
+    public void onPlaying() {
+
+    }
+
+    @Override
+    public void onPaused() {
+
+    }
+
+    @Override
+    public void onStopped() {
+
+    }
+
+    @Override
+    public void onBuffering(boolean b) {
+
+    }
+
+    @Override
+    public void onSeekTo(int i) {
+
+    }
+//    public void showToolbar(String title, boolean upButton){
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarApp);
+//        toolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.video_primary));
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setTitle(title);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+//    }
+
 }
